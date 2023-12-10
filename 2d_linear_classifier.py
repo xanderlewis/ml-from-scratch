ba8f14d304bb391d86d7647c271712617a26d202
xanderlewis <me@xanderlewis.co.uk>
2023-12-10 01:30:28 +0000

    Add better data and a plot

--- 2d_linear_classifier.py
+++ 2d_linear_classifier.py
@@ -1,6 +1,8 @@
 # A quick and dirty implementation of a linear classifier.
 
 import random
+import numpy as np # (for random number generation)
+from matplotlib import pyplot as plt
 from termcolor import cprint
 
 class BinaryLinearClassifier:
@@ -42,32 +44,68 @@ def round_prediction(p):
 	else:
 		return 0
 
+# (Ideally we want to have two sets of points that are actually linearly separable)
+def generate_points(n, std_dev=0.2):
+	"""Returns a 2D 'point cloud' consisting of two randomly-generated clusters of n points in the given square."""
+	# Centered at (loc1, loc1) and (loc2, loc2)
+	loc1 = -0.5
+	loc2 = 0.5
+	class1 = []
+	class2 = []
+
+	for i in range(n):
+		class1.append(list(rng.normal(loc1, std_dev, 2)))
+		class2.append(list(rng.normal(loc2, std_dev, 2)))
+
+	return class1 + class2
+
+rng = np.random.default_rng()
+
+# [HARDCODED TRAINING SAMPLES BELOW]
+
 # Data (points in the unit square; six of each class)
-points = [
-	[0.1, 0.2], [0.23, 0.3], [0.2, 0.65], [0.35, 0.45], [0.4, 0.15], [0.48, 0.32],
-	[0.4, 0.8], [0.6, 0.7], [0.6, 0.6], [0.7, 0.85], [0.75, 0.75], [0.95, 0.9]]
+#points = [
+	#[0.1, 0.2], [0.23, 0.3], [0.2, 0.65], [0.35, 0.45], [0.4, 0.15], [0.48, 0.32],
+	#[0.4, 0.8], [0.6, 0.7], [0.6, 0.6], [0.7, 0.85], [0.75, 0.75], [0.95, 0.9]
+	#]
 
 # Rescale points to lie in [-1, 1] x [-1, 1]
-for point in points:
-	for i in point:
-		i = 2 * i - 1
+#for point in points:
+	#for i in point:
+		#i = 2 * i - 1
 
+#true_classes = [
+	#0, 0, 0, 0, 0, 0,
+	#1, 1, 1, 1, 1, 1]
 
-true_classes = [
-	0, 0, 0, 0, 0, 0,
-	1, 1, 1, 1, 1, 1]
+# Data parameters
+NUM_EACH_CLASS = 100
+STANDARD_DEVIATION = 0.2
 
-NUM_DATA_POINTS = len(true_classes)
-
-# Hyperparameters
+# Model hyperparameters
 EPOCHS = 300
 LEARNING_RATE = 0.1
+
+# Generate training (and, in this case, testing) data
+points = generate_points(n=NUM_EACH_CLASS, std_dev=STANDARD_DEVIATION)
+true_classes = [0] * NUM_EACH_CLASS + [1] * NUM_EACH_CLASS
+
+# Plot distribution of points
+plt.scatter([p[0] for p in points], [p[1] for p in points])
+plt.show()
+# LATER: plot the initial (randomly initialised) and final decision boundaries.
+# (plot w1 * x1 + w2 * x2 + b = 0.5)
 
 # Create our model
 model = BinaryLinearClassifier()
 
+# Save the initial model parameters
+initial_w1 = model.w1
+initial_w2 = model.w2
+initial_b = model.b
+
 # -- Training loop --
-# We'll do 'full batch' training. (i.e. 'batch size' is equal to the number of training examples)'
+# We'll do 'full batch' training. (i.e. 'batch size' is equal to the number of training examples)
 epochs_done = 0
 
 while(True):
@@ -90,11 +128,13 @@ while(True):
 	print('true classes:      ', true_classes)
 
 	# Count the number of correct predictions and calculate a percentage
-	percent_accuracy = [i == j for i, j in zip(predicted_classes, true_classes)].count(True) * 100 / NUM_DATA_POINTS
-	print(f'accuracy: {percent_accuracy:.1f}%')
+	percent_accuracy = [i == j for i, j in zip(predicted_classes, true_classes)].count(True) * 100 / (NUM_EACH_CLASS * 2)
+	cprint(f'accuracy: {percent_accuracy:.2f}%', 'blue')
 
 	if percent_accuracy == 100.0:
 		cprint(f'Converged after {epochs_done} epochs. 👏🏻', attrs=['bold'])
+		cprint(f'\nInitial model parameters: \nw1 = {initial_w1} \nw2 = {initial_w2} \nb = {initial_b}', 'yellow')
+		cprint(f'\nFinal model parameters: \nw1 = {model.w1} \nw2 = {model.w2} \nb = {model.b}', 'magenta')
 		break
 
 	print('weight 1: ', model.w1)
@@ -113,3 +153,5 @@ while(True):
 	model.b -= LEARNING_RATE * g_b
 
 	epochs_done += 1
+
+	#input()
